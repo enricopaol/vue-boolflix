f86a603d7cd10cdcc4121a3902a27edf9e9a78c4
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-29 16:40:57 +0200

    added flags and tv shows

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@ var app = new Vue(
         data: {
             api_key: 'eb9246517e88c3bbd5a354470a892d61',
             searchQuery: '',
-            resultMovies: []            
+            resultMovies: [],
+            resultTvShows: [],
+            flagImages: {
+                en: 'https://lipis.github.io/flag-icon-css/flags/4x3/gb.svg',
+                it: 'https://lipis.github.io/flag-icon-css/flags/4x3/it.svg',
+                pl: 'https://lipis.github.io/flag-icon-css/flags/4x3/pl.svg',
+                fr: 'https://lipis.github.io/flag-icon-css/flags/4x3/fr.svg',
+                es: 'https://lipis.github.io/flag-icon-css/flags/4x3/es.svg',   
+                de: 'https://lipis.github.io/flag-icon-css/flags/4x3/de.svg',
+                da: 'https://lipis.github.io/flag-icon-css/flags/4x3/dk.svg',
+                sv: 'https://lipis.github.io/flag-icon-css/flags/4x3/se.svg',
+                tr: 'https://lipis.github.io/flag-icon-css/flags/4x3/tr.svg'                           
+            }            
+
         },
         methods: {
             searchMovie() {
@@ -18,11 +31,23 @@ var app = new Vue(
                     })
                     .then((response) => {
                         const result = response.data.results;
-                        console.log(result)
-
-                        this.resultMovies = result;
-                    })                                        
-            }
-        }
+                        this.resultMovies = result;                                                
+                    })                    
+            },
+            searchTvShows() {
+                axios
+                    .get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            api_key: this.api_key,
+                            query: this.searchQuery,
+                            language: 'it-IT'
+                        }
+                    })
+                    .then((response) => {
+                        const result = response.data.results;
+                        this.resultTvShows = result;                                                                        
+                    }) 
+            }            
+        }        
     }
 );
