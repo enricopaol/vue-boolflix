178db57d2a32a70d6256af8daae56100b1f0852f
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-29 13:36:23 +0200

    Mostro risultati di ricerca base

--- js/script.js
+++ js/script.js
@@ -2,10 +2,27 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            api_key: 'eb9246517e88c3bbd5a354470a892d61',
+            searchQuery: '',
+            resultMovies: []            
         },
         methods: {
+            searchMovie() {
+                axios
+                    .get('https://api.themoviedb.org/3/search/movie', {
+                        params: {
+                            api_key: this.api_key,
+                            query: this.searchQuery,
+                            language: 'it-IT'
+                        }
+                    })
+                    .then((response) => {
+                        const result = response.data.results;
+                        console.log(result)
 
+                        this.resultMovies = result;
+                    })                                        
+            }
         }
     }
 );
